31980a4272550b87b564bccc01c0f54184cc51de
Kai Sassnowski <sassnowski@wycomco.de>
2018-05-17 15:17:50 +0200

    Update package description

--- README.md
+++ README.md
@@ -7,9 +7,7 @@ Python 3.5+
 ## Installation
 
 ```bash
-git clone git@github.com:ksassnowski/kimai-cli.git
-cd kimai-cli
-pip install .
+pip install kimai-cli
 ```
 
 You can now use the `kimai` command in your shell. Before first use you need to run `kimai configure`.
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='Kimai CLI',
-    version='0.1.0',
+    name='kimai-cli',
+    version='0.2.0',
+    author='Kai Sassnowski',
+    author_email='me@kai-sassnowski.com',
     py_modules=['cli', 'kimai', 'config'],
     python_requires='>3.5.2',
     install_requires=[
