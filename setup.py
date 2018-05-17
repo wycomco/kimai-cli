8c9d0fc7f59057762722d8737434b2e009b3809e
Kai Sassnowski <sassnowski@wycomco.de>
2018-05-17 09:47:58 +0200

    Improve setup.py to include requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+import os
 from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='Kimai CLI',
     version='0.1',
-    py_modules=['cli'],
-    install_requires=[
-        'Click',
-    ],
+    py_modules=['cli', 'kimai', 'config'],
+    install_requires=required,
     entry_points='''
         [console_scripts]
         kimai=cli:cli
