46ca20aef463e03dcf0c5b6c482939bffe2720a2
Kai Sassnowski <sassnowski@wycomco.de>
2018-05-17 14:41:30 +0200

    Add proper version number to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Kimai CLI',
-    version='0.1',
+    version='0.1.0',
     py_modules=['cli', 'kimai', 'config'],
     python_requires='>3.5.2',
     install_requires=[
