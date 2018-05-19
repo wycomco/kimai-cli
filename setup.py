b9214148114f1c25ae0103a7974ec3bdcd5c2717
Kai Sassnowski <ksassnowski@gmail.com>
2018-05-19 17:03:40 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kimai-cli',
-    version='0.5.1',
+    version='0.5.2',
     author='Kai Sassnowski',
     author_email='me@kai-sassnowski.com',
     scripts=['bin/kimai-complete.sh'],
