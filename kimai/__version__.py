6f2141755fcf943cab7df7c74668eaf17a9a09ed
Kai Sassnowski <ksassnowski@gmail.com>
2018-05-23 23:04:54 +0200

    Bump version number

--- kimai/__version__.py
+++ kimai/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'kimai-cli'
 __description__ = 'A command line interface for the Kimai time tracking software.'
 __url__ = 'https://github.com/wycomco/kimai-cli'
-__version__ = '0.5.3'
+__version__ = '0.6.0'
 __author__ = 'Kai Sassnowski'
 __author_email__ = 'me@kai-sassnowski.com'
 __license__ = 'MIT'
